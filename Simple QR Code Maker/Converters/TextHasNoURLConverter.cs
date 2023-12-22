95524d79bb6db32b314d3bd0f2f29d68ebcf56d0
Joseph Finney <josephfinney@LIVE.COM>
2023-12-22 13:04:27 -0600

    Warn when QR Code does not have a URL

---
+++ Simple QR Code Maker/Converters/TextHasNoURLConverter.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.UI.Xaml.Data;
+
+namespace Simple_QR_Code_Maker.Converters;
+internal class TextHasNoURLConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, string language)
+    {
+        if (value is string text && Uri.IsWellFormedUriString(text, UriKind.Absolute))
+            return false;
+
+        return true;
+    }
+    public object ConvertBack(object value, Type targetType, object parameter, string language) => throw new NotImplementedException();
+}
