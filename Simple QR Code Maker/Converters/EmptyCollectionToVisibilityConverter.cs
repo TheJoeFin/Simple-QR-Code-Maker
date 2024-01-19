cda23921e9d8051381505939e230bbefe827e323
Joseph Finney <josephfinney@LIVE.COM>
2024-01-19 17:03:34 -0600

    Add converter back, but use param to invert logic

---
+++ Simple QR Code Maker/Converters/EmptyCollectionToVisibilityConverter.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Data;
+
+namespace Simple_QR_Code_Maker.Converters;
+
+class EmptyCollectionToVisibilityConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, string language)
+    {
+        if (parameter is "true")
+        {
+            if (value is not 0)
+                return Visibility.Visible;
+
+            return Visibility.Collapsed;
+        }
+
+        if (value is 0)
+            return Visibility.Visible;
+
+        return Visibility.Collapsed;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, string language) => throw new NotImplementedException();
+}
