6a018c6bf04a75b653b4933438cd305121ee36e6
Joseph Finney <TheJoeFin@users.noreply.github.com>
2024-01-20 17:25:37 -0600

    use deferral when dragging to ensure package is ready

--- Simple QR Code Maker/Views/MainPage.xaml.cs
+++ Simple QR Code Maker/Views/MainPage.xaml.cs
@@ -1,8 +1,8 @@
 ﻿using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Media.Imaging;
-using Simple_QR_Code_Maker.Helpers;
 using Simple_QR_Code_Maker.Extensions;
+using Simple_QR_Code_Maker.Helpers;
 using Simple_QR_Code_Maker.ViewModels;
 using Windows.ApplicationModel.DataTransfer;
 using Windows.Storage;
@@ -32,18 +32,23 @@ public sealed partial class MainPage : Page
         if (sender is not Image image || image.Source is not WriteableBitmap bitmap)
             return;
 
+        DragOperationDeferral deferral = args.GetDeferral();
         StorageFolder folder = ApplicationData.Current.LocalCacheFolder;
         string? imageNameFileName = $"{ToolTipService.GetToolTip(image)}" ?? "QR_Code";
-        // remove charcters that are not allowed in file names
+        // remove characters that are not allowed in file names
         imageNameFileName = imageNameFileName.ReplaceReservedCharacters();
         imageNameFileName += ".png";
         StorageFile file = await folder.CreateFileAsync(imageNameFileName, CreationCollisionOption.ReplaceExisting);
         bool success = await bitmap.SavePngToStorageFile(file);
 
         if (!success)
+        {
+            deferral.Complete();
             return;
+        }
 
         args.Data.SetStorageItems(new[] { file });
         args.Data.RequestedOperation = DataPackageOperation.Copy;
+        deferral.Complete();
     }
 }
