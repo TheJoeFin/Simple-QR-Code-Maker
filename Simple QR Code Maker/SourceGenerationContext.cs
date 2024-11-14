ea18f44ab3775754843ca5bb37d7c96ed29d4343
Joseph Finney <josephfinney@live.com>
2024-11-13 20:54:59 -0600

    add JSON serializing context

---
+++ Simple QR Code Maker/SourceGenerationContext.cs
@@ -0,0 +1,13 @@
+﻿using Simple_QR_Code_Maker.Models;
+using System.Text.Json.Serialization;
+
+[JsonSourceGenerationOptions(WriteIndented = true)]
+[JsonSerializable(typeof(HistoryItem))]
+[JsonSerializable(typeof(DecodingImageItem))]
+[JsonSerializable(typeof(TextBorderInfo))]
+[JsonSerializable(typeof(LocalSettingsOptions))]
+[JsonSerializable(typeof(BarcodeImageItem))]
+[JsonSerializable(typeof(ErrorCorrectionOptions))]
+internal partial class SourceGenerationContext : JsonSerializerContext
+{
+}
