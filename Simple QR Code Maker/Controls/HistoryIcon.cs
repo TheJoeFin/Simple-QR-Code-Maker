8e4f82b610c8002741f3ff21bd7d23aff1920f87
Joseph Finney <josephfinney@live.com>
2025-03-07 19:56:19 -0600

    Use IconControls instead of mystry glyphs

---
+++ Simple QR Code Maker/Controls/HistoryIcon.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Media;
+
+namespace Simple_QR_Code_Maker.Controls;
+
+public partial class HistoryIcon : FontIcon
+{
+	public HistoryIcon()
+	{
+		this.FontFamily = (FontFamily)Application.Current.Resources["SymbolThemeFontFamily"];
+		this.Glyph = "\uE81C";
+	}
+}
+
