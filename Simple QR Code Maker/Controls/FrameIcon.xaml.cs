f24f9a23a38fb5da02d34efa187809fab6692c64
Joe Finney <josephfinney@LIVE.COM>
2026-04-26 18:16:22 -0500

    Add FrameIcon user control with flipped CalendarDay icon
    Introduced a new FrameIcon user control consisting of a SymbolIcon using the "CalendarDay" symbol, vertically flipped via ScaleTransform. Included corresponding code-behind for initialization.

---
+++ Simple QR Code Maker/Controls/FrameIcon.xaml.cs
@@ -0,0 +1,11 @@
+using Microsoft.UI.Xaml.Controls;
+
+namespace Simple_QR_Code_Maker.Controls;
+
+public sealed partial class FrameIcon : UserControl
+{
+    public FrameIcon()
+    {
+        this.InitializeComponent();
+    }
+}
