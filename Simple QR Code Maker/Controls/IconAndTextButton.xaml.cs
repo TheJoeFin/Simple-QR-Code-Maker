3ea7e08d22f1ef98b2168fa0d0adce4c39627c96
Joe Finney <josephfinney@LIVE.COM>
2026-04-26 10:16:20 -0500

    Refactor IconAndTextButton to support toggle mode
    Refactored IconAndTextButton from a Button to a UserControl that supports both standard and checkable (toggle) button modes, exposing new properties for IsCheckable, IsChecked, IsThreeState, Command, and CommandParameter. Updated event handling and dependency properties for improved binding. Also updated FaqsContent.xaml to use {Binding} for better ItemsRepeater compatibility.

--- Simple QR Code Maker/Controls/IconAndTextButton.xaml.cs
+++ Simple QR Code Maker/Controls/IconAndTextButton.xaml.cs
@@ -1,9 +1,10 @@
+using System.Windows.Input;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 
 namespace Simple_QR_Code_Maker.Controls;
 
-public sealed partial class IconAndTextButton : Button
+public sealed partial class IconAndTextButton : UserControl
 {
     public IconAndTextButton()
     {
@@ -17,7 +18,7 @@ public sealed partial class IconAndTextButton : Button
     }
 
     public static readonly DependencyProperty TextProperty =
-        DependencyProperty.Register("Text", typeof(string), typeof(IconAndTextButton), new PropertyMetadata(""));
+        DependencyProperty.Register(nameof(Text), typeof(string), typeof(IconAndTextButton), new PropertyMetadata(string.Empty, OnBindablePropertyChanged));
 
     public UIElement Icon
     {
@@ -26,6 +27,77 @@ public sealed partial class IconAndTextButton : Button
     }
 
     public static readonly DependencyProperty IconProperty =
-        DependencyProperty.Register("Icon", typeof(UIElement), typeof(IconAndTextButton), new PropertyMetadata(null));
+        DependencyProperty.Register(nameof(Icon), typeof(UIElement), typeof(IconAndTextButton), new PropertyMetadata(null, OnBindablePropertyChanged));
+
+    public bool IsCheckable
+    {
+        get { return (bool)GetValue(IsCheckableProperty); }
+        set { SetValue(IsCheckableProperty, value); }
+    }
+
+    public static readonly DependencyProperty IsCheckableProperty =
+        DependencyProperty.Register(nameof(IsCheckable), typeof(bool), typeof(IconAndTextButton), new PropertyMetadata(false, OnBindablePropertyChanged));
+
+    public bool? IsChecked
+    {
+        get { return (bool?)GetValue(IsCheckedProperty); }
+        set { SetValue(IsCheckedProperty, value); }
+    }
+
+    public static readonly DependencyProperty IsCheckedProperty =
+        DependencyProperty.Register(nameof(IsChecked), typeof(bool?), typeof(IconAndTextButton), new PropertyMetadata(false, OnBindablePropertyChanged));
+
+    public bool IsThreeState
+    {
+        get { return (bool)GetValue(IsThreeStateProperty); }
+        set { SetValue(IsThreeStateProperty, value); }
+    }
+
+    public static readonly DependencyProperty IsThreeStateProperty =
+        DependencyProperty.Register(nameof(IsThreeState), typeof(bool), typeof(IconAndTextButton), new PropertyMetadata(false, OnBindablePropertyChanged));
+
+    public ICommand? Command
+    {
+        get { return (ICommand?)GetValue(CommandProperty); }
+        set { SetValue(CommandProperty, value); }
+    }
+
+    public static readonly DependencyProperty CommandProperty =
+        DependencyProperty.Register(nameof(Command), typeof(ICommand), typeof(IconAndTextButton), new PropertyMetadata(null, OnBindablePropertyChanged));
+
+    public object? CommandParameter
+    {
+        get { return GetValue(CommandParameterProperty); }
+        set { SetValue(CommandParameterProperty, value); }
+    }
+
+    public static readonly DependencyProperty CommandParameterProperty =
+        DependencyProperty.Register(nameof(CommandParameter), typeof(object), typeof(IconAndTextButton), new PropertyMetadata(null, OnBindablePropertyChanged));
+
+    public bool IsNotCheckable => !this.IsCheckable;
+
+    public event RoutedEventHandler? Click;
+
+    public event RoutedEventHandler? Checked;
+
+    public event RoutedEventHandler? Unchecked;
+
+    public event RoutedEventHandler? Indeterminate;
+
+    private static void OnBindablePropertyChanged(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs _)
+    {
+        IconAndTextButton control = (IconAndTextButton)dependencyObject;
+        control.Bindings.Update();
+    }
+
+    private void StandardButton_Click(object sender, RoutedEventArgs e) => this.Click?.Invoke(this, e);
+
+    private void CheckableButton_Click(object sender, RoutedEventArgs e) => this.Click?.Invoke(this, e);
+
+    private void CheckableButton_Checked(object sender, RoutedEventArgs e) => this.Checked?.Invoke(this, e);
+
+    private void CheckableButton_Unchecked(object sender, RoutedEventArgs e) => this.Unchecked?.Invoke(this, e);
+
+    private void CheckableButton_Indeterminate(object sender, RoutedEventArgs e) => this.Indeterminate?.Invoke(this, e);
 
 }
